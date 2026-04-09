7171d1bffdbf7f127a6d3c0b4b2a4314acd5181a
ishimweghislain <ishimweghislain82@gmail.com>
2026-04-09 17:11:36 +0200

    Deletd

--- check_notifs.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'config.php';
-$stmt = $pdo->query("SELECT * FROM notifications");
-$res = $stmt->fetchAll();
-print_r($res);
-?>
--- check_settings.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'config.php';
-$stmt = $pdo->query("SELECT * FROM settings");
-print_r($stmt->fetchAll());
-?>
